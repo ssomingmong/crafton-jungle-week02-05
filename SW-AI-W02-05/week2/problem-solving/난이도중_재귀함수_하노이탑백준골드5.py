ea3e773c0572ced372bf3f15554dc6aeed760bf0
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-10 20:45:28 +0900

    1914 solve

--- SW-AI-W02-05/week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
+++ SW-AI-W02-05/week2/problem-solving/난이도중_재귀함수_하노이탑백준골드5.py
@@ -1,2 +1,16 @@
 # 재귀함수 - 하노이 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1914
+
+def hanoi(n, start, mid, end):
+    if n == 1:
+        print(start, end)
+        return
+    
+    hanoi(n - 1, start, end, mid)
+    print(start, end) 
+    hanoi(n - 1, mid, start, end)
+    
+N = int(input())
+print(2**N - 1)
+if N <= 20:
+    hanoi(N, 1, 2, 3)
