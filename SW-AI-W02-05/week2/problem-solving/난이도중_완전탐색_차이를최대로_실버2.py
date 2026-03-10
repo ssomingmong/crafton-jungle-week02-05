767a3dc6aeaf451141a31825eafb2dc4966bcc84
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-10 16:38:12 +0900

    10819 풀이 성공

--- SW-AI-W02-05/week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
+++ SW-AI-W02-05/week2/problem-solving/난이도중_완전탐색_차이를최대로_실버2.py
@@ -1,15 +1,28 @@
 # 완전탐색 - 차이를 최대로 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/10819
 
-
 N = int(input())
-
 arr = list(map(int, input().split()))
-min = sorted(arr)
-max = sorted(arr, reverse=True)
-result = 0
-for i in range(len(arr)):
-    for j in range(len(arr) - 1):
-        result += (-1) * (min[i] - max[i])
 
-print(result)
+result = []
+used = [False] * N
+
+def permutation(current):
+    if len(current) == N:
+        sum = 0
+        for i in range(N - 1):
+            sum += abs(current[i] - current[i+1])
+        result.append(sum)
+        return
+    
+    for i in range(N):
+        if not used[i]:
+            used[i] = True
+            current.append(arr[i])
+            permutation(current)
+            
+            current.pop()
+            used[i] = False
+
+permutation([])
+print(max(result))
