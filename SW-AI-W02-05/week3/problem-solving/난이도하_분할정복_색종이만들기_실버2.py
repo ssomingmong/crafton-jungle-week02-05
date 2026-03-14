7b8c53c068a0cb8cf79030620e2e2e31c989585a
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-14 17:26:49 +0900

    slove: basic, 색종이 만들기

--- SW-AI-W02-05/week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_분할정복_색종이만들기_실버2.py
@@ -1,2 +1,31 @@
 # 분할정복 - 색종이 만들기 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/2630
+
+N = int(input())
+paper = [list(map(int, input().split())) for _ in range(N)]
+
+white = 0
+blue = 0
+def divide(x, y, size):
+    global white, blue
+    color = paper[x][y]
+
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if paper[i][j] != color:
+                half = size // 2
+                divide(x, y, half)
+                divide(x + half, y, half)
+                divide(x, y + half, half)
+                divide(x + half, y + half, half)
+
+                return
+    if color == 0:
+        white += 1
+    else:
+        blue += 1
+
+    
+divide(0, 0, N)
+print(white)
+print(blue)
