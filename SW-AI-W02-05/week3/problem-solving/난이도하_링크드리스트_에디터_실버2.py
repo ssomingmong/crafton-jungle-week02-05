ecca1b15e3aef7d379ce8eb5dc5e59c3e83a6508
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-15 18:28:52 +0900

    solve: boj-1406

--- SW-AI-W02-05/week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -1,7 +1,30 @@
 # 링크드리스트 - 에디터 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1406
+
 import sys
 
-string = sys.stdin.readline()
+input = sys.stdin.readline
 
-print(string)
+left_stack = list(input().strip())
+right_stack = []
+m = int(input())
+
+for _ in range(m):
+    command = input().split()
+    
+    if command[0] == 'L':
+        if left_stack:
+            right_stack.append(left_stack.pop())
+            
+    elif command[0] == 'D':
+        if right_stack:
+            left_stack.append(right_stack.pop())
+            
+    elif command[0] == 'B':
+        if left_stack:
+            left_stack.pop()
+            
+    elif command[0] == 'P':
+        left_stack.append(command[1])
+
+print("".join(left_stack + right_stack[::-1]))
