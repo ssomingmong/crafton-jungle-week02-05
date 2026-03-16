2a0d49ab1527ff8e2dd6aa1de0b987c1aab1e8d9
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-16 13:52:07 +0900

    solve:boj-1629-곱셈

--- SW-AI-W02-05/week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ SW-AI-W02-05/week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,21 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+import sys, math
+
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+
+def power(a, b, c):
+    if b == 1:
+        return a % c
+    
+    half = power(a, b // 2, c)
+
+    if b % 2 == 0:
+        return (half * half) % c
+    else:
+        return (half * half * a) %c
+    
+print(power(a, b, c))
