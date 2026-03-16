4d3e6025db23feaec0f8edc9fed658150dd61bec
Mincheol Kim <kimmincheol99@gmail.com>
2026-03-16 16:24:58 +0900

    solve:boj-2493-탑

--- SW-AI-W02-05/week3/problem-solving/난이도중_스택_탑_골드5.py
+++ SW-AI-W02-05/week3/problem-solving/난이도중_스택_탑_골드5.py
@@ -1,2 +1,22 @@
 # 스택 - 탑 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/2493
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+height = list(map(int, input().split()))
+stack = []
+result = [0] * N
+
+for i in range(N):
+
+    while stack and stack[-1][1] < height[i]:
+        stack.pop()
+
+    if stack:
+        result[i] = stack[-1][0] + 1
+
+    stack.append((i, height[i]))
+
+print(*result)
